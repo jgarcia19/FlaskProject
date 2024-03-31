e97c1e4f761da2f73d32f60a4d9d8db9700dc1b4
jgarcia19 <jasong2000@gmail.com>
2024-03-30 20:48:22 -0400

    Added add_task REST API

--- Hello.py
+++ Hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
@@ -12,16 +12,46 @@ tasks = client.get_database('test').get_collection('tasks')
 
 @app.route('/')
 def hello_world():
-   return "Hello World"
 
-@app.route('/documents')
+   hw = "Hello World"
+
+   response = jsonify(hw)
+   response.status_code = 200
+
+   return response
+
+@app.route('/tasks')
 def documents(): 
    documents = tasks.find({})
 
-   for document in documents:
-      print(document)
+   data = []
 
-   return "done"
+   for document in documents:
+      task = {}
+      task["task"] = document["task"]
+      data.append(task)
+
+   response = jsonify(data)
+
+   response.status_code = 200
+
+   return response
+
+@app.route('/tasks/add', methods=['POST'])
+def add_task():
+   data = request.json
+
+   print(data)
+
+   tasks.insert_one(data)
+
+
+   message = "Task Successfully Added"
+   response = jsonify(message)
+   response.status_code = 200
+   return response
+
+
 
 if __name__ == '__main__':
    app.run()
