c0d400d1c70d4306fb1ee3186b30fcbd2826a343
jgarcia19 <jasong2000@gmail.com>
2024-04-01 17:01:36 -0400

    Added get all Airbnb REST API

--- api/Airbnb.py
+++ api/Airbnb.py
@@ -3,8 +3,36 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 
-Airbnb_api = Blueprint('airbnb_api', __name__)
+airbnb_api = Blueprint('airbnb_api', __name__)
 
 load_dotenv()
 database_url = os.getenv("DATABASE_URL")
 client = MongoClient(database_url)
+airbnbs = client.get_database('sample_airbnb').get_collection('listingsAndReviews')
+
+
+@airbnb_api.route('/airbnb')
+def documents():
+    documents = airbnbs.find({})
+
+    data = []
+
+    i = 0
+    for document in documents:
+        if i == 100:
+            break
+        airbnb = {}
+        airbnb["name"] = document["name"]
+        airbnb["summary"] = document["summary"]
+        airbnb["space"] = document["space"]
+        airbnb["description"] = document["description"]
+        data.append(airbnb)
+
+        i += 1
+        
+
+    response = jsonify(data)
+
+    response.status_code = 200
+
+    return response
--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
 from dotenv import load_dotenv
-import api.Tasks
+from api import Tasks, Airbnb
 import os
 
 app = Flask(__name__)
 
-app.register_blueprint(api.Tasks.tasks_api)
+app.register_blueprint(Tasks.tasks_api)
+app.register_blueprint(Airbnb.airbnb_api)
 
 @app.route('/')
 def hello_world():
