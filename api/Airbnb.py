db96cd0521d79fd163f61957ca2f9853fea95e26
jgarcia19 <jasong2000@gmail.com>
2024-04-03 14:16:06 -0400

    Added Search Endpoint

--- api/Airbnb.py
+++ api/Airbnb.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+import re
 
 airbnb_api = Blueprint('airbnb_api', __name__)
 
@@ -11,7 +12,7 @@ client = MongoClient(database_url)
 airbnbs = client.get_database('sample_airbnb').get_collection('listingsAndReviews')
 
 
-@airbnb_api.route('/airbnb')
+@airbnb_api.route('/airbnb', methods=['GET'])
 def documents():
     documents = airbnbs.find({})
 
@@ -28,6 +29,40 @@ def documents():
         airbnb["description"] = document["description"]
         data.append(airbnb)
 
+        i += 1
+        
+
+    response = jsonify(data)
+
+    response.status_code = 200
+
+    return response
+
+
+@airbnb_api.route('/airbnb/search', methods=['GET'])
+def search_by_name_regex():
+
+    pattern = request.args.get('search')
+
+    if pattern == None:
+        return jsonify({"error": "Please Enter a Query in the URL!"}), 400
+    
+    regex = re.compile(pattern)
+
+    documents = airbnbs.find({"name": {"$regex": regex}})
+
+    data = []
+
+    i = 0
+    for document in documents:
+        if i == 100:
+            break
+        airbnb = {}
+        airbnb["name"] = document["name"]
+        airbnb["summary"] = document["summary"]
+        airbnb["space"] = document["space"]
+        airbnb["description"] = document["description"]
+        data.append(airbnb)
         i += 1
         
 
